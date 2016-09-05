f8ba175f028d2e831578815204bf170a471fe7cc
yeyincai <yeyincai@ppmoney.com>
2016-09-05 20:19:26 +0800

    grpc simple demo

---
+++ grpc-demo-simple/src/main/java/com/grpc/demo/simple/SimpleClientStart.java
@@ -0,0 +1,40 @@
+package com.grpc.demo.simple;
+
+
+import com.google.protobuf.StringValue;
+import com.yyc.grpc.contract.SayHelloResponse;
+import com.yyc.grpc.contract.SimpleServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NettyChannelBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleClientStart {
+
+    private ManagedChannel managedChannel;
+    private int PORT = 8888;
+
+    private void createChannel(){
+        managedChannel = NettyChannelBuilder.forAddress("localhost",PORT).usePlaintext(true).build();
+    }
+
+    private void shutdown(){
+        if(managedChannel!=null){
+            try {
+                managedChannel.shutdown().awaitTermination(2, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleClientStart simpleClientStart = new SimpleClientStart();
+        simpleClientStart.createChannel();
+        SimpleServiceGrpc.SimpleServiceBlockingStub simpleServiceStub = SimpleServiceGrpc.newBlockingStub(simpleClientStart.managedChannel);
+
+        SayHelloResponse sayHelloResponse = simpleServiceStub.sayHello(StringValue.newBuilder().setValue("grpc-simple-demo").build());
+        System.out.println("response:"+sayHelloResponse.getResult());
+        simpleClientStart.shutdown();
+    }
+}
---
+++ grpc-demo-simple/src/main/java/com/grpc/demo/simple/SimpleServer.java
@@ -0,0 +1,18 @@
+package com.grpc.demo.simple;
+
+import com.yyc.grpc.contract.SayHelloResponse;
+import com.yyc.grpc.contract.SimpleServiceGrpc;
+
+/**
+ * Created by yeyc on 2016/9/5.
+ */
+public class SimpleServer extends SimpleServiceGrpc.SimpleServiceImplBase{
+
+    @Override
+    public void sayHello(com.google.protobuf.StringValue request,
+                         io.grpc.stub.StreamObserver<com.yyc.grpc.contract.SayHelloResponse> responseObserver) {
+        SayHelloResponse sayHelloResponse = SayHelloResponse.newBuilder().setResult(request.getValue().concat("hello world")).build();
+        responseObserver.onNext(sayHelloResponse);
+        responseObserver.onCompleted();
+    }
+}
